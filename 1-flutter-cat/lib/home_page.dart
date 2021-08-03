b753dfbc95de4c60b40542fc0807d94935893280
Param Siddharth <contact@paramsid.com>
2021-08-03 15:42:19 +0530

    Day 2 completed

---
+++ 1-flutter-cat/lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final num day = 2;
+  final String name = 'Param';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalogue'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to day ${day.toStringAsFixed(0)} of $name\'s Flutter application!'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
