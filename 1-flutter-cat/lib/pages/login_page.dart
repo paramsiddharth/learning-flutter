e291354cb3bf0c2e436f6222353fe4c8c213800b
Param Siddharth <contact@paramsid.com>
2021-08-04 19:02:28 +0530

    दिवस ४ पूर्ण हुआ

--- 1-flutter-cat/lib/pages/login_page.dart
+++ 1-flutter-cat/lib/pages/login_page.dart
@@ -4,17 +4,66 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          'Login page.',
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
           ),
-          textScaleFactor: 2,
-        ),
-      ),
+          Image.asset(
+            'assets/images/login.png',
+            fit: BoxFit.cover,
+            
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Welcome!',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter username',
+                    labelText: 'Username'
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter password',
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text('Login'),
+                  onPressed: () {
+                    print('Hi, friends!');
+                  },
+                  /* style: TextButton.styleFrom(
+                    textStyle: TextStyle(
+                      color: Colors.white
+                    )
+                  ) */
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
